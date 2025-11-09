52d0fde94f8f272ded5b580371aa41bff89576c4
WofWca <wofwca@protonmail.com>
2025-11-09 14:07:15 +0400

    refactor: add `cg_oldGibs` CVAR, `CG_GibPlayerOld`
    This is in preparation to some gibs improvements.

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -152,6 +152,7 @@ vmCvar_t	cg_bobroll;
 vmCvar_t	cg_swingSpeed;
 vmCvar_t	cg_shadows;
 vmCvar_t	cg_gibs;
+vmCvar_t	cg_oldGibs;
 vmCvar_t	cg_drawTimer;
 vmCvar_t	cg_drawFPS;
 vmCvar_t	cg_drawSnapshot;
@@ -310,6 +311,7 @@ static cvarTable_t cgameCvarTable[] = {
 	{ &cg_viewsize, "cg_viewsize", "100", CVAR_ARCHIVE, RANGE_INT( 30, 100 ) },
 	{ &cg_shadows, "cg_shadows", "1", CVAR_ARCHIVE, RANGE_ALL  },
 	{ &cg_gibs, "cg_gibs", "1", CVAR_ARCHIVE, RANGE_BOOL },
+	{ &cg_oldGibs, "cg_oldGibs", "0", CVAR_ARCHIVE, RANGE_BOOL },
 	{ &cg_draw2D, "cg_draw2D", "1", CVAR_ARCHIVE, RANGE_BOOL },
 	{ &cg_drawStatus, "cg_drawStatus", "1", CVAR_ARCHIVE, RANGE_BOOL },
 	{ &cg_drawTimer, "cg_drawTimer", "0", CVAR_ARCHIVE, RANGE_BOOL },
