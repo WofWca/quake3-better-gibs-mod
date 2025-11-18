fa5689fd071c73d099555bfc6d12ce5750b11b58
WofWca <wofwca@protonmail.com>
2025-11-18 19:14:07 +0400

    improvement: lower gibs bounceFactor
    This makes the gibs feel more "dead" and less frog-like.

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -321,7 +321,7 @@ static cvarTable_t cgameCvarTable[] = {
 	{ &cg_gibsInheritPlayerVelocity, "cg_gibsInheritPlayerVelocity", "1.0", CVAR_ARCHIVE, RANGE_ALL },
 	{ &cg_gibsExtraRandomVelocity, "cg_gibsExtraRandomVelocity", "250", CVAR_ARCHIVE, RANGE_ALL },
 	{ &cg_gibsExtraVerticalVelocity, "cg_gibsExtraVerticalVelocity", "100", CVAR_ARCHIVE, RANGE_ALL },
-	{ &cg_gibsBounceFactor, "cg_gibsBounceFactor", "0.6", CVAR_ARCHIVE, RANGE_ALL },
+	{ &cg_gibsBounceFactor, "cg_gibsBounceFactor", "0.4", CVAR_ARCHIVE, RANGE_ALL },
 	{ &cg_draw2D, "cg_draw2D", "1", CVAR_ARCHIVE, RANGE_BOOL },
 	{ &cg_drawStatus, "cg_drawStatus", "1", CVAR_ARCHIVE, RANGE_BOOL },
 	{ &cg_drawTimer, "cg_drawTimer", "0", CVAR_ARCHIVE, RANGE_BOOL },
