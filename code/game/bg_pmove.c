51dd17f95f1b1d0c09821b89d7b80c635df6229e
WofWca <wofwca@protonmail.com>
2026-01-27 18:49:38 +0400

    refactor: rename `*_HEIGHT` -> `*_MAXS_Z`
    This is a follow-up to 38c41807405aaac8e99b1200e74059e2e2ea05bb
    (https://github.com/ioquake/ioq3/pull/828).
    
    This is not really "height" because height is constituted by
    `maxs[2] - mins[2]`.
    So the new name should be in line with the existing variable
    named `MINS_Z`.

--- code/game/bg_pmove.c
+++ code/game/bg_pmove.c
@@ -1283,7 +1283,7 @@ static void PM_CheckDuck (void)
 
 	if (pm->ps->pm_type == PM_DEAD)
 	{
-		pm->ps->maxs[2] = DEAD_HEIGHT;
+		pm->ps->maxs[2] = DEAD_MAXS_Z;
 		pm->ps->viewheight = DEAD_VIEWHEIGHT;
 		return;
 	}
@@ -1297,7 +1297,7 @@ static void PM_CheckDuck (void)
 		if (pm->ps->pm_flags & PMF_DUCKED)
 		{
 			// try to stand up
-			pm->ps->maxs[2] = DEFAULT_HEIGHT;
+			pm->ps->maxs[2] = MAXS_Z;
 			pm->trace (&trace, pm->ps->origin, pm->ps->mins, pm->ps->maxs, pm->ps->origin, pm->ps->clientNum, pm->tracemask );
 			if (!trace.allsolid)
 				pm->ps->pm_flags &= ~PMF_DUCKED;
@@ -1306,12 +1306,12 @@ static void PM_CheckDuck (void)
 
 	if (pm->ps->pm_flags & PMF_DUCKED)
 	{
-		pm->ps->maxs[2] = CROUCH_HEIGHT;
+		pm->ps->maxs[2] = CROUCH_MAXS_Z;
 		pm->ps->viewheight = CROUCH_VIEWHEIGHT;
 	}
 	else
 	{
-		pm->ps->maxs[2] = DEFAULT_HEIGHT;
+		pm->ps->maxs[2] = MAXS_Z;
 		pm->ps->viewheight = DEFAULT_VIEWHEIGHT;
 	}
 }
--- code/game/g_combat.c
+++ code/game/g_combat.c
@@ -615,7 +615,7 @@ void player_die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
 
 	self->s.loopSound = 0;
 
-	self->client->ps.maxs[2] = self->s.maxs[2] = DEAD_HEIGHT;
+	self->client->ps.maxs[2] = self->s.maxs[2] = DEAD_MAXS_Z;
 
 	// don't allow respawn until the death anim is done
 	// g_forcerespawn may force spawning at some later time
