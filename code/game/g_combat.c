d73966bdba0a487faebaee6f901908107d1b285d
WofWca <wofwca@protonmail.com>
2026-05-09 13:47:52 +0400

    fix: gib random speed after match end being lower

--- code/game/g_combat.c
+++ code/game/g_combat.c
@@ -278,22 +278,30 @@ void GibEntity( gentity_t *self, int killer, const int damageBloodFallback ) {
 	// we're simply providing the client with the knockback info,
 	// and whether to use that into is up to `cg_oldGibs`.
 	if ( g_gibsNewEvGibPlayerParmProtocol.integer == 1 ) {
-		int damage;
+		int damage = damageBloodFallback;
 		float knockbackSpeed;
 
-		// We prefer actual damage over `client->damage_knockback`
-		// because `damage_knockback` is sometimes undesirably 0. Namely:
-		// - when the target is a dead body, with `FL_NO_KNOCKBACK`.
-		// - when the knockback `dir` is not provided to `G_Damage`,
-		//   such as with crushers.
-		//
-		// Most of the time (but not always e.g. with lava)
-		// "no knockback" means "the player should not be moved
-		// in any particular direction",
-		// and not that "their gibs should stay put".
-		damage = self->client
-			? self->client->damage_blood + self->client->damage_armor
-			: damageBloodFallback;
+		if ( self->client ) {
+			// We prefer actual damage over `client->damage_knockback`
+			// because `damage_knockback` is sometimes undesirably 0. Namely:
+			// - when the target is a dead body, with `FL_NO_KNOCKBACK`.
+			// - when the knockback `dir` is not provided to `G_Damage`,
+			//   such as with crushers.
+			//
+			// Most of the time (but not always e.g. with lava)
+			// "no knockback" means "the player should not be moved
+			// in any particular direction",
+			// and not that "their gibs should stay put".
+			const int frameDamage =
+				self->client->damage_blood + self->client->damage_armor;
+			// In case when `level.intermissionQueued` we don't use
+			// `damage_blood` and `damage_armor` because they would not be set.
+			// See `level.intermissionQueued` and `targ->die == body_die` check
+			// in `G_Damage`.
+			if ( !level.intermissionQueued || frameDamage != 0 ) {
+				damage = frameDamage;
+			}
+		}
 		if ( damage > MAX_KNOCKBACK ) {
 			damage = MAX_KNOCKBACK;
 		}
