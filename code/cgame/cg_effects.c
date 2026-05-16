78deeff100d0b6b9f5ebe25c7c9a428ca864e6fb
WofWca <wofwca@protonmail.com>
2026-05-16 19:12:48 +0400

    improvement: more uniform spread, less randomness

--- code/cgame/cg_effects.c
+++ code/cgame/cg_effects.c
@@ -736,14 +736,18 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorCopy( baseOrigin, origin );
 		VectorMA(origin, MINS_Z + 0.95 * playerHeight, up, origin);
 		VectorClear( velocity );
-		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
-		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, 0.5*Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
+		VectorMA( velocity, 0.5*Q_crandom(&seed)*baseRandomVelocity, right, velocity );
 		// For the skull / brain we want the random velocity
 		// to never have downwards (inwards) component,
-		// so we use `Q_random` instead of `Q_crandom`.
+		// so we use `Q_random` instead of `Q_crandom`
+		// (and less randomness overall).
 		// We also do the same for other gibs,
 		// but for the left / right velocity components.
-		VectorMA( velocity, Q_random(&seed)*baseRandomVelocity, up, velocity );
+		VectorMA( velocity,
+			(0.25 + 0.5 * (Q_random(&seed) + Q_random(&seed))/2)
+				*baseRandomVelocity,
+			up, velocity );
 		velocity[2] += jump;
 		VectorAdd( velocity, playerVelocityScaled, velocity );
 		if ( !skullLaunched && (Q_rand(&seed) & 1) ) {
@@ -773,7 +777,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, 0.8 * playerRadius, right, origin );
 		VectorMA( origin, -0.3 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, +Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, +(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
@@ -836,7 +840,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, -0.6 * playerRadius, right, origin );
 		VectorMA( origin, +0.2 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, -Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, -(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
@@ -866,7 +870,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, 0.5 * playerRadius, right, origin );
 		VectorMA( origin, 0.1 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, +Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, +(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
@@ -884,7 +888,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, -0.5 * playerRadius, right, origin );
 		VectorMA( origin, -0.2 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, -Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, -(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
@@ -904,7 +908,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, -0.7 * playerRadius, right, origin );
 		VectorMA( origin, -0.2 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, -Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, -(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
@@ -924,7 +928,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, +0.5 * playerRadius, right, origin );
 		VectorMA( origin, +0.0 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, +Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, +(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
@@ -942,7 +946,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, -0.5 * playerRadius, right, origin );
 		VectorMA( origin, -0.5 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, -Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, -(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
@@ -981,7 +985,7 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		VectorMA( origin, +0.9 * playerRadius, right, origin );
 		VectorMA( origin, -0.2 * playerRadius, forward, origin );
 		VectorClear( velocity );
-		VectorMA( velocity, +Q_random(&seed)*baseRandomVelocity, right, velocity );
+		VectorMA( velocity, +(0.25+0.5*Q_random(&seed))*baseRandomVelocity, right, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, forward, velocity );
 		VectorMA( velocity, Q_crandom(&seed)*baseRandomVelocity, up, velocity );
 		velocity[2] += jump;
