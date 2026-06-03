4b81a30f111142113ba4e7f004eb4079a64c467e
WofWca <wofwca@protonmail.com>
2026-06-03 14:04:52 +0400

    refactor: move `EV_GIB_PLAYER` handling to fn

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -1410,6 +1410,9 @@ void CG_ScorePlum( int client, const vec3_t origin, int score );
 void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 				const vec3_t playerVelocity, const int knockbackSpeed,
 				const lerpFrame_t *bodyAnimation, const int randSeed );
+// Same as `CG_GibPlayer`, but takes different params.
+void CG_GibPlayer2( const centity_t *cent, const entityState_t *es,
+					const clientInfo_t *ci );
 void CG_GibPlayerOld( const vec3_t playerOrigin );
 void CG_BigExplode( vec3_t playerOrigin );
 
