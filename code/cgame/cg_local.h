13c2ebc3f6d5deffe12420d42c8a18d1db91d314
WofWca <wofwca@protonmail.com>
2026-06-14 23:37:56 +0400

    feat: `cg_gibsOriginalOrigin` CVAR (default to 0)

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -1106,6 +1106,7 @@ extern	vmCvar_t		cg_gibsExtraRandomVelocity;
 extern	vmCvar_t		cg_gibsRandomVelocityFromKnockback;
 extern	vmCvar_t		cg_gibsVerticalVelocityFromKnockback;
 extern	vmCvar_t		cg_gibsExtraVerticalVelocity;
+extern	vmCvar_t		cg_gibsOriginalOrigin;
 extern	vmCvar_t		cg_gibsBounceFactor;
 extern	vmCvar_t		cg_gibsBounceFactorRandomness;
 extern	vmCvar_t		cg_gibsRotationFactor;
