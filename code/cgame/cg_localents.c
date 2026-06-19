9ff2413a5bd286c69fc5e96bebf7aa0b889cd88f
WofWca <wofwca@protonmail.com>
2026-06-19 23:38:21 +0400

    feat: `cg_gibsKnockback`

--- code/cgame/cg_localents.c
+++ code/cgame/cg_localents.c
@@ -1011,7 +1011,7 @@ void CG_AddInvulnerabilityJuiced( localEntity_t *le ) {
 			randSeed = Q_rand(&randSeed) + cgs.levelStartTime;
 
 			CG_GibPlayer( le->refEntity.origin, angles, le->pos.trDelta,
-				knockbackSpeed, NULL, randSeed );
+				NULL, knockbackSpeed, NULL, randSeed );
 		}
 	}
 	else {
