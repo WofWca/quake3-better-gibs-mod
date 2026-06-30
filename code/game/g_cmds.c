01324855ebc7f4ac33d918810a9866cbb48dd355
WofWca <wofwca@protonmail.com>
2026-06-30 13:48:03 +0400

    refactor: add `dir` arg to `die()` fn

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -480,7 +480,7 @@ void Cmd_Kill_f( gentity_t *ent ) {
 	}
 	ent->flags &= ~FL_GODMODE;
 	ent->client->ps.stats[STAT_HEALTH] = ent->health = -999;
-	player_die (ent, ent, ent, 100000, MOD_SUICIDE);
+	player_die (ent, ent, ent, NULL, 100000, MOD_SUICIDE);
 }
 
 /*
@@ -612,7 +612,7 @@ void SetTeam( gentity_t *ent, const char *s ) {
 		// Kill him (makes sure he loses flags, etc)
 		ent->flags &= ~FL_GODMODE;
 		ent->client->ps.stats[STAT_HEALTH] = ent->health = 0;
-		player_die (ent, ent, ent, 100000, MOD_SUICIDE);
+		player_die (ent, ent, ent, NULL, 100000, MOD_SUICIDE);
 
 	}
 
