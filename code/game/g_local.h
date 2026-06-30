f21f3023aefe0ae52ea2db93494cac22749d44a7
WofWca <wofwca@protonmail.com>
2026-06-30 16:58:03 +0400

    fix: gibs: apply knockback to dead bodies

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -503,7 +503,7 @@ const char *BuildShaderStateConfig( void );
 //
 qboolean CanDamage (gentity_t *targ, vec3_t origin);
 void G_Damage (gentity_t *targ, gentity_t *inflictor, gentity_t *attacker, vec3_t dir, vec3_t point, int damage, int dflags, int mod);
-void GibEntity( gentity_t *self, int killer, const int damageBloodFallback );
+void GibEntity( gentity_t *self, int killer, const vec3_t dir, const int damageBloodFallback );
 // The shotgun does `G_Damage` multiple times, per each pellet.
 // Normally that would mean that if the target is at 1 HP,
 // only one pellet would hit them.
@@ -755,6 +755,7 @@ extern	vmCvar_t	g_blood;
 extern	vmCvar_t	g_oldGibs;
 extern	vmCvar_t	g_gibsMissileDirectionKnockbackWeight;
 extern	vmCvar_t	g_gibsNewEvGibPlayerProtocol;
+extern	vmCvar_t	g_gibsDeadBodyLinearVelocityFromKnockback;
 extern	vmCvar_t	g_gibsOnCollisionInheritPlayerVelocity;
 extern	vmCvar_t	g_gibsOnCollisionMinSpeed;
 extern	vmCvar_t	g_gibsOnCollisionBaseDamage;
