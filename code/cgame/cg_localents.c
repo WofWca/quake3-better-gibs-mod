fca7b358ad126e64b5719fafad7938e8c5fba97c
WofWca <wofwca@protonmail.com>
2026-07-10 15:34:28 +0400

    feat: launch an extra brain when gibbing Klesk

--- code/cgame/cg_localents.c
+++ code/cgame/cg_localents.c
@@ -1011,7 +1011,7 @@ void CG_AddInvulnerabilityJuiced( localEntity_t *le ) {
 			randSeed = Q_rand(&randSeed) + cgs.levelStartTime;
 
 			CG_GibPlayer( le->refEntity.origin, angles, le->pos.trDelta,
-				NULL, knockbackSpeed, NULL, randSeed );
+				NULL, knockbackSpeed, NULL, NULL, randSeed );
 		}
 	}
 	else {
