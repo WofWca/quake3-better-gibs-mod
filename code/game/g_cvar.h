7b4aa446495c1fb44d0b05a63eb3447043b6f38a
WofWca <wofwca@protonmail.com>
2026-07-23 23:58:46 +0400

    refactor: clarify `trackChange` in `g_cvar.h`
    So that you don't have to go to declaration
    every time you want to figure out what this `qfalse` and `qtrue`
    is referring to.

--- code/game/g_cvar.h
+++ code/game/g_cvar.h
@@ -43,87 +43,94 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 	extern	vmCvar_t	var;
 #endif
 
-// don't override the cheat state set by the system
-G_CVAR( g_cheats, "sv_cheats", "", 0, qfalse )
+// trackChange
+#define TRACK qtrue
+// Don't trackChange
+#define NO_TRACK qfalse
 
-G_CVAR( g_restarted, "g_restarted", "0", CVAR_ROM, qfalse )
+// don't override the cheat state set by the system
+G_CVAR( g_cheats, "sv_cheats", "", 0, NO_TRACK )
+
+G_CVAR( g_restarted, "g_restarted", "0", CVAR_ROM, NO_TRACK )
 
 // latched vars
-G_CVAR( g_gametype, "g_gametype", "0", CVAR_SERVERINFO | CVAR_USERINFO | CVAR_LATCH, qfalse )
+G_CVAR( g_gametype, "g_gametype", "0", CVAR_SERVERINFO | CVAR_USERINFO | CVAR_LATCH, NO_TRACK )
 
 // allow this many total, including spectators
-G_CVAR( g_maxclients, "sv_maxclients", "8", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, qfalse )
+G_CVAR( g_maxclients, "sv_maxclients", "8", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, NO_TRACK )
 // allow this many active
-G_CVAR( g_maxGameClients, "g_maxGameClients", "0", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, qfalse )
+G_CVAR( g_maxGameClients, "g_maxGameClients", "0", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, NO_TRACK )
 
 // change anytime vars
-G_CVAR( g_dmflags, "dmflags", "0", CVAR_SERVERINFO | CVAR_ARCHIVE, qtrue )
-G_CVAR( g_fraglimit, "fraglimit", "20", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, qtrue )
-G_CVAR( g_timelimit, "timelimit", "0", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, qtrue )
-G_CVAR( g_capturelimit, "capturelimit", "8", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, qtrue )
+G_CVAR( g_dmflags, "dmflags", "0", CVAR_SERVERINFO | CVAR_ARCHIVE, TRACK )
+G_CVAR( g_fraglimit, "fraglimit", "20", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, TRACK )
+G_CVAR( g_timelimit, "timelimit", "0", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, TRACK )
+G_CVAR( g_capturelimit, "capturelimit", "8", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, TRACK )
 
-G_CVAR( g_synchronousClients, "g_synchronousClients", "0", CVAR_SYSTEMINFO, qfalse )
+G_CVAR( g_synchronousClients, "g_synchronousClients", "0", CVAR_SYSTEMINFO, NO_TRACK )
 
-G_CVAR( g_friendlyFire, "g_friendlyFire", "0", CVAR_ARCHIVE, qtrue )
+G_CVAR( g_friendlyFire, "g_friendlyFire", "0", CVAR_ARCHIVE, TRACK )
 
-G_CVAR( g_teamAutoJoin, "g_teamAutoJoin", "0", CVAR_ARCHIVE, qfalse )
-G_CVAR( g_teamForceBalance, "g_teamForceBalance", "0", CVAR_ARCHIVE, qfalse )
+G_CVAR( g_teamAutoJoin, "g_teamAutoJoin", "0", CVAR_ARCHIVE, NO_TRACK )
+G_CVAR( g_teamForceBalance, "g_teamForceBalance", "0", CVAR_ARCHIVE, NO_TRACK )
 
-G_CVAR( g_warmup, "g_warmup", "20", CVAR_ARCHIVE, qtrue )
-G_CVAR( g_doWarmup, "g_doWarmup", "0", CVAR_ARCHIVE, qtrue )
-G_CVAR( g_logfile, "g_log", "games.log", CVAR_ARCHIVE, qfalse )
-G_CVAR( g_logfileSync, "g_logsync", "0", CVAR_ARCHIVE, qfalse )
+G_CVAR( g_warmup, "g_warmup", "20", CVAR_ARCHIVE, TRACK )
+G_CVAR( g_doWarmup, "g_doWarmup", "0", CVAR_ARCHIVE, TRACK )
+G_CVAR( g_logfile, "g_log", "games.log", CVAR_ARCHIVE, NO_TRACK )
+G_CVAR( g_logfileSync, "g_logsync", "0", CVAR_ARCHIVE, NO_TRACK )
 
-G_CVAR( g_password, "g_password", "", CVAR_USERINFO, qfalse )
+G_CVAR( g_password, "g_password", "", CVAR_USERINFO, NO_TRACK )
 
-G_CVAR( g_banIPs, "g_banIPs", "", CVAR_ARCHIVE, qfalse )
-G_CVAR( g_filterBan, "g_filterBan", "1", CVAR_ARCHIVE, qfalse )
+G_CVAR( g_banIPs, "g_banIPs", "", CVAR_ARCHIVE, NO_TRACK )
+G_CVAR( g_filterBan, "g_filterBan", "1", CVAR_ARCHIVE, NO_TRACK )
 
-G_CVAR( g_needpass, "g_needpass", "0", CVAR_SERVERINFO | CVAR_ROM, qfalse )
+G_CVAR( g_needpass, "g_needpass", "0", CVAR_SERVERINFO | CVAR_ROM, NO_TRACK )
 
-G_CVAR( g_dedicated, "dedicated", "0", 0, qfalse )
+G_CVAR( g_dedicated, "dedicated", "0", 0, NO_TRACK )
 
-G_CVAR( g_speed, "g_speed", "320", 0, qtrue )
-G_CVAR( g_gravity, "g_gravity", "800", 0, qtrue )
-G_CVAR( g_knockback, "g_knockback", "1000", 0, qtrue )
-G_CVAR( g_quadfactor, "g_quadfactor", "3", 0, qtrue )
-G_CVAR( g_weaponRespawn, "g_weaponrespawn", "5", 0, qtrue )
-G_CVAR( g_weaponTeamRespawn, "g_weaponTeamRespawn", "30", 0, qtrue )
-G_CVAR( g_forcerespawn, "g_forcerespawn", "20", 0, qtrue )
-G_CVAR( g_inactivity, "g_inactivity", "0", 0, qtrue )
-G_CVAR( g_debugMove, "g_debugMove", "0", 0, qfalse )
-G_CVAR( g_debugDamage, "g_debugDamage", "0", 0, qfalse )
-G_CVAR( g_debugAlloc, "g_debugAlloc", "0", 0, qfalse )
-G_CVAR( g_motd, "g_motd", "", 0, qfalse )
-G_CVAR( g_blood, "com_blood", "1", 0, qfalse )
+G_CVAR( g_speed, "g_speed", "320", 0, TRACK )
+G_CVAR( g_gravity, "g_gravity", "800", 0, TRACK )
+G_CVAR( g_knockback, "g_knockback", "1000", 0, TRACK )
+G_CVAR( g_quadfactor, "g_quadfactor", "3", 0, TRACK )
+G_CVAR( g_weaponRespawn, "g_weaponrespawn", "5", 0, TRACK )
+G_CVAR( g_weaponTeamRespawn, "g_weaponTeamRespawn", "30", 0, TRACK )
+G_CVAR( g_forcerespawn, "g_forcerespawn", "20", 0, TRACK )
+G_CVAR( g_inactivity, "g_inactivity", "0", 0, TRACK )
+G_CVAR( g_debugMove, "g_debugMove", "0", 0, NO_TRACK )
+G_CVAR( g_debugDamage, "g_debugDamage", "0", 0, NO_TRACK )
+G_CVAR( g_debugAlloc, "g_debugAlloc", "0", 0, NO_TRACK )
+G_CVAR( g_motd, "g_motd", "", 0, NO_TRACK )
+G_CVAR( g_blood, "com_blood", "1", 0, NO_TRACK )
 
-G_CVAR( g_podiumDist, "g_podiumDist", "80", 0, qfalse )
-G_CVAR( g_podiumDrop, "g_podiumDrop", "70", 0, qfalse )
+G_CVAR( g_podiumDist, "g_podiumDist", "80", 0, NO_TRACK )
+G_CVAR( g_podiumDrop, "g_podiumDrop", "70", 0, NO_TRACK )
 
-G_CVAR( g_allowVote, "g_allowVote", "1", CVAR_ARCHIVE, qfalse )
-G_CVAR( g_listEntity, "g_listEntity", "0", 0, qfalse )
+G_CVAR( g_allowVote, "g_allowVote", "1", CVAR_ARCHIVE, NO_TRACK )
+G_CVAR( g_listEntity, "g_listEntity", "0", 0, NO_TRACK )
 
 #ifdef MISSIONPACK
-G_CVAR( g_obeliskHealth, "g_obeliskHealth", "2500", 0, qfalse )
-G_CVAR( g_obeliskRegenPeriod, "g_obeliskRegenPeriod", "1", 0, qfalse )
-G_CVAR( g_obeliskRegenAmount, "g_obeliskRegenAmount", "15", 0, qfalse )
-G_CVAR( g_obeliskRespawnDelay, "g_obeliskRespawnDelay", "10", CVAR_SERVERINFO, qfalse )
+G_CVAR( g_obeliskHealth, "g_obeliskHealth", "2500", 0, NO_TRACK )
+G_CVAR( g_obeliskRegenPeriod, "g_obeliskRegenPeriod", "1", 0, NO_TRACK )
+G_CVAR( g_obeliskRegenAmount, "g_obeliskRegenAmount", "15", 0, NO_TRACK )
+G_CVAR( g_obeliskRespawnDelay, "g_obeliskRespawnDelay", "10", CVAR_SERVERINFO, NO_TRACK )
 
-G_CVAR( g_cubeTimeout, "g_cubeTimeout", "30", 0, qfalse )
-G_CVAR_EXT( g_redteam, "g_redteam", "Stroggs", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO , 0, qtrue, qtrue )
-G_CVAR_EXT( g_blueteam, "g_blueteam", "Pagans", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO , 0, qtrue, qtrue )
-G_CVAR( g_singlePlayer, "ui_singlePlayerActive", "", 0, qfalse )
+G_CVAR( g_cubeTimeout, "g_cubeTimeout", "30", 0, NO_TRACK )
+G_CVAR_EXT( g_redteam, "g_redteam", "Stroggs", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO , 0, TRACK, qtrue )
+G_CVAR_EXT( g_blueteam, "g_blueteam", "Pagans", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO , 0, TRACK, qtrue )
+G_CVAR( g_singlePlayer, "ui_singlePlayerActive", "", 0, NO_TRACK )
 
-G_CVAR( g_enableDust, "g_enableDust", "0", CVAR_SERVERINFO, qtrue )
-G_CVAR( g_enableBreath, "g_enableBreath", "0", CVAR_SERVERINFO, qtrue )
-G_CVAR( g_proxMineTimeout, "g_proxMineTimeout", "20000", 0, qfalse )
+G_CVAR( g_enableDust, "g_enableDust", "0", CVAR_SERVERINFO, TRACK )
+G_CVAR( g_enableBreath, "g_enableBreath", "0", CVAR_SERVERINFO, TRACK )
+G_CVAR( g_proxMineTimeout, "g_proxMineTimeout", "20000", 0, NO_TRACK )
 #endif
-G_CVAR( g_smoothClients, "g_smoothClients", "1", 0, qfalse )
-G_CVAR( pmove_fixed, "pmove_fixed", "0", CVAR_SYSTEMINFO, qfalse )
-G_CVAR( pmove_msec, "pmove_msec", "8", CVAR_SYSTEMINFO, qfalse )
+G_CVAR( g_smoothClients, "g_smoothClients", "1", 0, NO_TRACK )
+G_CVAR( pmove_fixed, "pmove_fixed", "0", CVAR_SYSTEMINFO, NO_TRACK )
+G_CVAR( pmove_msec, "pmove_msec", "8", CVAR_SYSTEMINFO, NO_TRACK )
 
-G_CVAR( g_rankings, "g_rankings", "0", 0, qfalse )
-G_CVAR( g_localTeamPref, "g_localTeamPref", "", 0, qfalse )
+G_CVAR( g_rankings, "g_rankings", "0", 0, NO_TRACK )
+G_CVAR( g_localTeamPref, "g_localTeamPref", "", 0, NO_TRACK )
 
 #undef G_CVAR
 #undef G_CVAR_EXT
+#undef TRACK
+#undef NO_TRACK
